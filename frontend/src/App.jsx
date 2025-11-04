7198011f81e8aac45ac232e5dc8af87a58477f56
Hermon <hg2397@edu.turku.fi>
2025-11-04 15:48:58 +0200

    using sats can render diffrent movies based on searched value

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
-import MovieCard from './components/MovieCard'
+import Home from "./pages/Home"
 
 function App() {
-  const movieNumber= 1;
   return (
     <>
-    {movieNumber===1 && <MovieCard movie={{title: "Hermon", release_date: "2024"}}/>}
+    <Home/>
     </>
-    
   )
 }
 
