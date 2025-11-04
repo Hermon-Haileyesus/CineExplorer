114af8ca9e28dcfda26da86bef9bc4313bf3ef88
Hermon <hg2397@edu.turku.fi>
2025-11-04 13:06:04 +0200

    this is how conditon statment made, short if case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import './App.css'
 import MovieCard from './components/MovieCard'
 
 function App() {
+  const movieNumber= 2;
   return (
     <>
-    <MovieCard movie={{title: "Hermon", release_date: "2024"}}/>
-    <MovieCard movie={{title: "Times", release_date: "2024"}}/>
+    {movieNumber===1 ?(<MovieCard movie={{title: "Hermon", release_date: "2024"}}/>):(<MovieCard movie={{title: "Times", release_date: "2024"}}/>)}
     </>
     
   )
